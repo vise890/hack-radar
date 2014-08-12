2c05dded0e7e24c694ba4b6dc246f6a95128d020
Martino Visintin <mvisinti@thoughtworks.com>
2014-08-12 20:27:05 +0200

    move db ns

---
+++ src/hack_radar/db/core.clj
@@ -0,0 +1,20 @@
+(ns hack-radar.db.core
+  (:require [monger.collection :as mc]
+            [monger.core :as mg]))
+
+;;; NOTE: a SQL db would have prb worked as well if not better
+;;; NOTE: using h2 would have removed the *massive* dep on mongo, but oh well
+;;;       this PJ is for trying new tech out
+
+(def db-name "rogueRadar")
+(def coll-name "techs")
+
+(def conn (mg/connect))
+(def db (mg/get-db conn db-name))
+
+(defn- drop-db []
+  (mg/drop-db conn db-name))
+
+(defn init-db []
+  (do (drop-db)
+      (mc/create db coll-name {})))
--- src/hack_radar/rogue_radar_seed.clj
+++ src/hack_radar/rogue_radar_seed.clj
@@ -1,28 +1,12 @@
 (ns hack-radar.rogue-radar-seed
   (:require [hack-radar.scraper :as scraper]
+            [hack-radar.db.core :as db]
             [monger.collection :as mc]
             [monger.core :as mg]))
 
-;;; NOTE: a SQL db would have prb worked as well if not better
-;;; NOTE: using h2 would have removed the *massive* dep on mongo, but oh well
-;;;       this PJ is for trying new tech out
-
-(def db-name "rougueRadar")
-(def coll-name "techs")
-
-(def conn (mg/connect))
-(def db (mg/get-db conn db-name))
-
-(defn- drop-db []
-  (mg/drop-db conn db-name))
-
-(defn- init-db [techs]
-  (do (mc/create db coll-name {})
-      (mc/insert-batch db coll-name techs)))
-
 (defn refresh-techs-db []
   (let [techs (scraper/scrape-tw-tech-radar)]
-       (drop-db)
-       (init-db techs)))
+       (db/init-db)
+       (mc/insert-batch db/db db/coll-name techs)))
 
 (refresh-techs-db)
