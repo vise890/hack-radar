e560da362a653aca97b7a7a708997b1faf32025b
Martino Visintin <mvisinti@thoughtworks.com>
2014-08-13 19:48:12 +0200

    reorder :area and :status possible values

--- src/hack_radar/db/tech.clj
+++ src/hack_radar/db/tech.clj
@@ -7,8 +7,14 @@
 (def Tech
      "A tech in the TW radar"
      {:name schema/Str
-      :status (schema/enum :adopt :assess :hold :trial)
-      :area (schema/enum :techniques :tools :platforms :languages-and-frameworks)
+      :status (schema/enum :adopt
+                           :trial
+                           :assess
+                           :hold)
+      :area (schema/enum :techniques
+                         :tools
+                         :platforms
+                         :languages-and-frameworks)
       :date schema/Str
       :url schema/Str})
 
